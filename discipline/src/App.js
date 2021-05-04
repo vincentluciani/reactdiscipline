2f023d860b552c5b5c381b6d3da4e0c5dfe1408a
Vincent Luciani <vincent.luciani@schneider-electric.com>
2021-05-04 09:02:20 +0200

    completed filter functionality

--- discipline/src/App.js
+++ discipline/src/App.js
@@ -4,15 +4,14 @@ import MyChart from './components/Chart'
 import Habits from './components/Habits'
 import React, { useState } from 'react'
 
-const habits_data = [{ 'date': new Date(2021, 4, 30), 'title': 'sleep well', 'progress': 66 },
-{ 'date': new Date(2021, 4, 28), 'title': 'eat well', 'progress': 10 }];
+const habits_data = [{ 'id': 1, 'date': new Date(2021, 4, 30), 'title': 'sleep well', 'progress': 66 },
+{ 'id': 2, 'date': new Date(2020, 4, 28), 'title': 'eat well', 'progress': 10 }];
 
 const App = () => {
 
   const [habits, setHabits] = useState(habits_data);
 
   const onNewItemHandler = habit => {
-    console.log(habit);
     setHabits((previousHabits) => { return [habit, ...previousHabits] });
   }
   return (
--- discipline/src/components/Habits.js
+++ discipline/src/components/Habits.js
@@ -1,15 +1,31 @@
 import './Habits.css'
 import Habit from './Habit'
 import Filter from './Filter'
+import React, { useState } from 'react'
 
 const Habits = (props) => {
+
+    const [displayYear, setYearChosen] = useState('')
+
+    const filteredItems = props.habits.filter(
+        habit => {
+            if (displayYear.length > 0) {
+                return habit.date.getFullYear().toString() === displayYear
+            }
+            else {
+                return true;
+            }
+        }
+    );
+
     const reactOnSelectedDate = yearChosen => {
+        setYearChosen(yearChosen);
         console.log("date selected:" + yearChosen)
     }
     return (
         <div>
             <Filter setDate={reactOnSelectedDate} />
-            {props.habits.map(habit => <Habit date={habit.date} title={habit.title} details={habit.details} progress={habit.progress} />)}
+            {filteredItems.map(habit => <Habit key={habit.id} date={habit.date} title={habit.title} details={habit.details} progress={habit.progress} />)}
         </div>
     );
 }
