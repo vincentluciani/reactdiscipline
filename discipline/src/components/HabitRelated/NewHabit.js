a0ba8f7b64e203718c84dfb159339b90aede1386
Vincent Luciani <vincent.luciani@schneider-electric.com>
2021-06-05 13:41:31 +0200

    Change the design of the link to Hide the new habit form

--- discipline/src/components/HabitRelated/NewHabit.js
+++ discipline/src/components/HabitRelated/NewHabit.js
@@ -4,6 +4,7 @@ import Box from '../Wrappers/Box'
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { IconContext } from "react-icons";
 import styles from './NewHabit.module.css';
+import SuperButton from '../Buttons/SuperButton'
 
 const NewHabit = (props) => {
 
@@ -26,7 +27,7 @@ const NewHabit = (props) => {
     }
 
     return (<React.Fragment>
-        { habitDisplay && <Box>< NewHabitForm onSaveMyItem={onSaveMyItemHandler} /> <div onClick={hideForm}>Hide</div></Box>}
+        { habitDisplay && <Box>< NewHabitForm onSaveMyItem={onSaveMyItemHandler} /><SuperButton onClick={hideForm}>Hide</SuperButton></Box>}
 
         { !habitDisplay &&
             <Box onClick={showForm} className={styles['new-habit-box']}><h2>New Item</h2><IconContext.Provider value={{ color: "#3e98c7", "size": "6em" }}><div> <BsFillPlusCircleFill /></div></IconContext.Provider></Box>}
